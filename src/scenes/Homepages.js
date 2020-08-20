9dfc9f6576505091a163c0d873e35ffabc63f3e0
AlfaRiza <malfariza45@gmail.com>
2020-08-21 00:37:44 +0700

    header

--- src/scenes/Homepages.js
+++ src/scenes/Homepages.js
@@ -1,7 +1,55 @@
 import React from "react";
+import Fade from 'react-reveal/Fade';
 import Header from "parts/Header";
+import Section from "elements/Section";
+import HeroImage from "assets/images/hero-image.png";
 export default function Homepages() {
-  return <div>
+  return (<div className="body-wrap">
     <Header></Header>
-  </div>;
+    <main className="site-content">
+      <Section className="hero illustration-section-01" isCenterContent>
+        <div className="container-sm">
+          <div className="hero-inner section-inner">
+            <div className="hero-content">
+              <Fade bottom delay={500}>
+                <h1
+                  className="mt-0 mb-16"
+                  data-reveal-delay="200"
+                >
+                  Landing template for startups
+              </h1>
+              </Fade>
+              <div className="container-xs">
+                <Fade bottom delay={1000}>
+                  <p
+                    className="mt-0 mb-32"
+                    data-reveal-delay="400"
+                  >
+                    Our landing page template works on all
+                    devices, so you only have to set it up
+                    once, and get beautiful results forever.
+									</p>
+                </Fade>
+              </div>
+            </div>
+            <div
+              className="hero-figure illustration-element-01"
+              data-reveal-value="20px"
+              data-reveal-delay="800"
+            >
+              <Fade bottom delay={1500}>
+                <img
+                  className="has-shadow"
+                  src={HeroImage}
+                  alt="Hero"
+                  width="896"
+                  height="504"
+                />
+              </Fade>
+            </div>
+          </div>
+        </div>
+      </Section>
+    </main>
+  </div>);
 }
